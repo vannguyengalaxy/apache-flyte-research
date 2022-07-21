27ea6ec183f86336ce8817743eac317a1f7e7500
EC2 Default User <ec2-user@ip-10-10-22-20.ap-southeast-1.compute.internal>
2022-07-21 09:35:50 +0000

    update configuration for select data from hudi s3 in aws

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ To get up and running with your Flyte project, we recommend following the
 2. We recommend using a git repository and this the ``docker_build_and_tag.sh``
    to build your docker images
 3. We also recommend using pip-compile to build your requirements.
+
+note:
+1. add dependences for test in local
+# "spark.jars": "/home/ec2-user/flyte-test/flyte-project/jars/hadoop-aws-3.3.1.jar,"
+#               "/home/ec2-user/flyte-test/flyte-project/jars/aws-java-sdk-bundle-1.11.375.jar,"
+#               "/home/ec2-user/flyte-test/flyte-project/jars/hudi-spark3.2-bundle_2.12-0.11.0.jar",
--- flyte/workflows/basicHudiQuery.py
+++ flyte/workflows/basicHudiQuery.py
@@ -1,7 +1,7 @@
-from flytekit import Resources, task, workflow
+from flytekit import Resources, task, workflow, kwtypes
 from flytekitplugins.spark import Spark
 import flytekit
-
+from typing import Dict
 @task(
     task_config=Spark(
         # this configuration is applied to the spark cluster
@@ -9,15 +9,21 @@ import flytekit
             "spark.driver.memory": "1000M",
             "spark.executor.memory": "1000M",
             "spark.executor.cores": "1",
-            "spark.executor.instances": "2",
+            "spark.executor.instances": "1",
             "spark.driver.cores": "1",
-            "spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.3.1,com.amazonaws:aws-java-sdk-bundle:1.11.375,org.apache.hudi:hudi-spark3.2-bundle_2.12:0.11.0"
+            "spark.jars": "local:///root/jars/hadoop-aws-3.3.1.jar,"
+                          "local:///root/jars/aws-java-sdk-bundle-1.11.375.jar,"
+                          "local:///root/jars/hudi-spark3.2-bundle_2.12-0.11.0.jar",
+            "spark.serializer": "org.apache.spark.serializer.KryoSerializer",
+            "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.hudi.catalog.HoodieCatalog",
+            "spark.sql.extensions": "org.apache.spark.sql.hudi.HoodieSparkSessionExtension"
+
         }
     ),
     limits=Resources(mem="2000M"),
     cache_version="1",
 )
-def hudiQery() -> str:
+def hudiQery() -> Dict[str, str]:
     # if you run in local, you must set SPARK_LOCAL_IP=127.0.0.1. this connection to get jars dependences local file
     spark = flytekit.current_context().spark_session
     # hadoop 3.3.1
@@ -26,19 +32,12 @@ def hudiQery() -> str:
     spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", "AKIAZZQUVIBN74RMCUXK")
     spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "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")
 
+    df = spark.read.format('org.apache.hudi').load('s3a://hudi-table/logs_mor' + '/*/*')
 
-    tripsSnapshotDF = spark. \
-        read. \
-        format("hudi"). \
-        load("s3a://hudi-table/logs_mor")
-
-    # tripsSnapshotDF.show()
-    pandasDF = tripsSnapshotDF.toPandas()
-    firstComitTime = pandasDF['_hoodie_commit_time'][0]
-    return firstComitTime
+    return df.select("_hoodie_commit_time").collect()[0].asDict()
 
 @workflow
-def my_query_spark() -> str:
+def my_query_spark() -> Dict[str, str]:
     return hudiQery()
 
 if __name__ == "__main__":
